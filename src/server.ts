014a075804ccbdc0ef9f2c9eb025af225c8caf43
jacky <giovanniarmaleo00@gmail.com>
2021-06-07 15:09:29 +0200

    start

--- src/server.ts
+++ src/server.ts
@@ -29,15 +29,15 @@ app.set('views', path.join(__dirname, 'view/'));
 
 app.listen(PORT, () => {
  	console.log(`⚡️: Server is running at https://localhost:${PORT}`);
-	files = init();
+	//files = init();
 })
 
 //every time the server is launched we have to read the stored files
 function init()
 {
-	var fileList :string[];//an array that contains all the file's name on the server
-	shell.ls(__dirname + "/files/").forEach(function(file, fileList){fileList.push(file)})
-	return fileList!;
+	//	var fileList :string[];//an array that contains all the file's name on the server
+	//shell.ls(__dirname + "/files/").forEach(function(file, fileList){fileList.push(file)})
+	//return fileList!;
 }
 
 export {files}
